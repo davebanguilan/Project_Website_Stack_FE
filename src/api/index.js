99eb002e7ce545f03745fab9cac98c40f2e234e2
davebanguilan <desbanguilan@gmail.com>
2021-01-18 00:25:27 +0800

    Redux, Axios added - Post Done

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const url = "http://localhost:8000/posts";
+
+export const fetchPosts = () => {
+  axios.get(url);
+};
+
+export const createPost = (newPost) => axios.post(url, newPost);
---
+++ src/reducers/posts.js
@@ -0,0 +1,12 @@
+//reducer is a function that accepts state and also action then
+
+export default (posts = [], action) => {
+  switch (action.type) {
+    case "FETCH_ALL":
+      return action.payload;
+    case "CREATE":
+      return [...posts, action.payload];
+    default:
+      return posts;
+  }
+};
